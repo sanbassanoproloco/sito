d7dfa48e03b1053158b148013f32fc350e0dcbce
sanbassanoproloco <sanbassanoproloco@gmail.com>
2026-08-24 22:09:31 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+ apiKey: "AIzaSyA9u8PWBjc90NuJ6_8n6nGhATkCPvDjKWc",
+  authDomain: "comunicazioni-17258.firebaseapp.com",
+  projectId: "comunicazioni-17258",
+  storageBucket: "comunicazioni-17258.firebasestorage.app",
+  messagingSenderId: "92685598278",
+  appId: "1:92685598278:web:82a1eddcb36ddfaaf4d447",
+  measurementId: "G-8L4VCKQX2B"
+};
